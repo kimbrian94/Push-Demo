799234b57f08c65fcadcfbe9913f43872453ed6f
Brian Kim <kim.brian94@gmail.com>
2020-09-08 10:31:31 -0400

    First

--- PushFun/AppDelegate.swift
+++ PushFun/AppDelegate.swift
@@ -7,31 +7,68 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseMessaging
+import FirebaseInstanceID
+import FirebaseCore
+import UserNotificationsUI
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        
+        
+        UNUserNotificationCenter.current().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+            options: authOptions,
+            completionHandler: {_, _ in })
+        
+        application.registerForRemoteNotifications()
+        
+        InstanceID.instanceID().instanceID { (result, error) in
+            if error != nil {
+                print(error?.localizedDescription)
+            } else {
+                if let token = result?.token {
+                    print("Token \(token)")
+                }
+            }
+        }
+        
         return true
     }
-
+    
+    // Some user opens the application through the tapping on the notification
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+        print("The user has tapped on a notification")
+    }
+    
+    // While user inside the app (in foreground)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("WE ARE IN THE APP")
+        
+        completionHandler([.alert,.badge,.sound])
+    }
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    
 }
 
--- PushFun/ViewController.swift
+++ PushFun/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        if let url = URL(string: "https://fcm.googleapis.com/v1/projects/pushfun-fbb1e/messages:send") {
+            var request = URLRequest(url: url)
+            request.allHTTPHeaderFields = ["Content-Type":"application/json", "Authorization":"Bearer " + "ya29.c.Kl7YByeARM4_gbhkFGUFYC-P2EnPTq71QQSslRURHiJ5ihsP7oBnxFdOiyyo1-ennLm_EkiL1KUatp8bjgzFLyGdpAkf6d31bnFo94zBYLnLr7yszd1PaNFDOUI4Lt-g"]
+            request.httpMethod = "POST"
+            request.httpBody = "{\"message\":{\"token\":\"ftu2y8mIc0X4l6Q6ZEVDsm:APA91bG2qiuNzNxcAQ0V-2DiTZKAODeC4R6Cp1Q-hWM6trclrqBH-jBqh7EgMpAZ0cYCUDp1jwFaTzMYJsY1G797ZOrwSjDZsrT9katfXwH_bswbsqa_GAmCQYM4Xemt4uv-7Wfmr0gf\",\"notification\":{\"body\":\"This is my first push.\",\"title\":\"First Push App\"}}}".data(using: .utf8)
+            
+            URLSession.shared.dataTask(with: request) { (data, urlresponse, error) in
+                if error != nil {
+                    print(error!)
+                } else {
+                    print(String(data: data!, encoding: .utf8)!)
+                }
+            }.resume()
+        }
     }
-
-
+    
+    
 }
 
